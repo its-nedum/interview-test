ad32b888b6856d42d5a2fec490f5c7a1d3c60a6c
its-nedum <de.magnificent1@gmail.com>
2021-05-28 21:27:26 +0100

    completed the task

--- question2.sql
+++ question2.sql
@@ -1,7 +1,7 @@
 /*
     The SQL query:
-        count(CITY) - return the number of all the table rows (N)
-        count(distinct CITY) - returns the number of all distinct row (N')
+        count(CITY) - return the number of all the city rows (N)
+        count(distinct CITY) - returns the number of all distinct city rows (N')
         and then (N') is subtracted from (N) and the difference (a number) will be returned
 
 */
